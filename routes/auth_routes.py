dc987e1a6403178b20d7cfe2c9bc64ffa7822665
shubhamk2253 <khots3636@gmail.com>
2026-02-22 00:41:13 +0530

    Update auth_routes.py

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import check_password_hash, generate_password_hash
-from extensions import db  # Ensure this matches your project structure
 from sqlalchemy import text
 from datetime import datetime, timedelta
 import random
@@ -149,3 +148,4 @@ def change_password():
     db.session.commit()
 
     return jsonify(success=True, message="Password updated successfully"), 200
+
