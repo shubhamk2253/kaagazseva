84bbef39752e67be122a9cf5c45ce3005442cbc7
shubhamk2253 <khots3636@gmail.com>
2026-02-23 20:22:59 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,10 @@
 from extensions import db
 from .user import User
+from .agent import Agent
 from .application import Application
-# When we create Agent, Payment, Wallet files, we import them here too.
+from .payment import Payment
+from .wallet import WalletTransaction
+from .ticket import Ticket
+from .service import Service
+from .state import State
+from .pincode import Pincode
