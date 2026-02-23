f6ec3b38e45d5efcca68a7a70c06e3d04d8636fe
shubhamk2253 <khots3636@gmail.com>
2026-02-23 20:12:39 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from extensions import db
+from .user import User
+from .application import Application
+# When we create Agent, Payment, Wallet files, we import them here too.
